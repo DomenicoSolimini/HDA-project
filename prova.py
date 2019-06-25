5e3682fa605259c0d6b33702e1fa60266b41ca23
DomenicoSolimini <52126523+DomenicoSolimini@users.noreply.github.com>
2019-06-25 14:18:34 +0200

    Si vedono le RD maps!!

--- prova.py
+++ prova.py
@@ -1,21 +1,14 @@
 import matplotlib.pyplot as plt
 import h5py
 import numpy as np
-from PIL import Image
-
-# dopo aver processato il file target3_126 eseguendo da terminale
-# python process.py --input <path_to_dataset>/train/target5_001.hdf5
+import process
+import argparse
 
 hdf = h5py.File("target3_126.hdf5",'r')
-list(hdf.keys()) # ['microdoppler', 'microdoppler_thresholded', 'radar', 'range_doppler']
+x = np.array(hdf['radar'])
+n_frame = x.shape[0]
 
-x =  hdf['microdoppler']
-print('shape = ', x.shape, '\nclass = ', type(x), '\n type = ', x.dtype)
-# il mio output è
-# shape =  (179, 256)
-# class =  <class 'numpy.ndarray'>
-#  type =  float32
-
-x = np.array(x)
-plt.plot(x)
-plt.show()
+ind = np.random.randint(0, n_frame)
+rda = process.range_doppler(x[ind])
+ind
+plt.matshow(rda)
